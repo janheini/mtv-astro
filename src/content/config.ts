427971ff3d5c1a4d4e82e55a742d42d67d4b725a
Jan Heinemeyer <janheinemeyer99@gmail.com>
2023-07-23 17:39:43 +0200

    posts -> berichte

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,7 +2,7 @@
 import { z, defineCollection } from 'astro:content';
 
 // 2. Define a `type` and `schema` for each collection
-const postCollection = defineCollection({
+const berichte = defineCollection({
   type: 'content', // v2.5.0 and later
   schema: z.object({
     title: z.string(),
@@ -14,5 +14,5 @@ const postCollection = defineCollection({
 
 // 3. Export a single `collections` object to register your collection(s)
 export const collections = {
-  'posts': postCollection,
+  'berichte': berichte
 };
