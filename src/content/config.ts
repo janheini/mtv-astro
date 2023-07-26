91c301042397c12a9c63f45af643b4ece2f150f1
Jan Heinemeyer <janheinemeyer99@gmail.com>
2023-07-26 07:21:47 +0200

    zod: date as union of string and date

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,7 +6,7 @@ const berichte = defineCollection({
   type: 'content', // v2.5.0 and later
   schema: z.object({
     title: z.string(),
-    date: z.string().optional(),
+    date: z.union([z.string(), z.date()]).optional(),
     tags: z.array(z.string()).optional(),
     image: z.string().optional(),
   }),
