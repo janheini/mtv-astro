18fde3928455e564164026c3f39f01cf3483cfca
Jan Heinemeyer <janheinemeyer99@gmail.com>
2023-09-28 12:50:32 +0200

    daisyui navbar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,4 +13,22 @@ module.exports = {
         require("@tailwindcss/typography"),
         require("daisyui")
     ],
+    daisyui: {
+        themes: [
+            {
+                mtv: {
+                    "primary": "#365314",
+                    "secondary": "#facc15",
+                    "accent": "#feec00",
+                    "neutral": "#171212",
+                    "base-100": "#f9fcf7",
+                    "info": "#a3e635",
+                    "success": "#579933",
+                    "warning": "#fbbf24",
+                    "error": "#f87272",
+                },
+            },
+
+        ],
+    },
 };
