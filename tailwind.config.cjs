ffc68c22aac027667433e0bf3c0b25e0287afe1c
Jan Heinemeyer <janheinemeyer99@gmail.com>
2023-09-28 11:19:43 +0200

    add daisyui

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#6c8644",
-        background: "#f9fcf7",
-      },
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#6c8644",
+                background: "#f9fcf7",
+            },
+        },
     },
-  },
-  plugins: [require("@tailwindcss/typography")],
+    plugins: [
+        require("@tailwindcss/typography"),
+        require("daisyui")
+    ],
 };
